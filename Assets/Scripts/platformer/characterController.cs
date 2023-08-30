3287737092458e9a502644354694e7c445df4a57
TTFGamesStudio <a_brand_new_@outlook.com>
2023-08-29 20:03:34 -0500

    Started Work
    I set up the basic room and added the first platformer area, the player is able to run back and forth

---
+++ Assets/Scripts/platformer/characterController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_Controller : MonoBehaviour
+{
+    [SerializeField] private CharacterController _controller;
+
+    [SerializeField] private Vector2 _input;
+
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private Animator anim;
+
+    [SerializeField] private Rigidbody rb;
+
+    [SerializeField] private float jumpForce;
+
+    [SerializeField] private float gravity;
+
+    [SerializeField] private Vector3 velocity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateInput();
+        updateVelocity();
+        _controller.Move(velocity);
+        updateAnimations();
+    }
+
+    void updateInput()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        _input = new Vector2(x,y);
+    }
+
+    void updateVelocity()
+    {
+        velocity.z = (moveSpeed * _input.x * Time.deltaTime) * -1f;
+        velocity.y += Time.deltaTime * gravity;
+        velocity.x = 0;
+    }
+
+    void updateAnimations()
+    {
+        if (_input.x > 0.1f || _input.x < -0.1f)
+        {
+            anim.SetBool("run",true);
+        }
+        else
+        {
+            anim.SetBool("run",false);
+        }
+    }
+}
